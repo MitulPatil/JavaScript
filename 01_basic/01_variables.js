0b0e0ae2f8c8c5f17b4d065c377992f249a32e35
Mitul Patil <119641618+MitulPatil@users.noreply.github.com>
2024-02-07 12:36:56 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=13234
+let accountEmail="abc@gmail.com"
+var accountPassWord="27643"
+accountCity="vadodara"
+let accountState
+
+// accountId=2 //not allowed
+
+accountEmail="ksdlgmail.com"
+accountPassword="6546546"
+accountCity="suart"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassWord,accountCity,accountState])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer verson
+
+//alter( 3 + 3 ) we are using ndoejs,not browser
+
+let name="mitul"
+let age=18
+let Islogin=false
+
+//types of data types in javascript
+
+/*  1) number => 2 to power 53
+    2) bigint
+    3) string => ""
+    4) boolean => true/false
+    5) null => standalone value 
+    6) undefined =>
+    7) symbol => unique
+
+    8) object
+*/
+
+console.log(typeof age)
+console.log(typeof undefined);  //typeof undefined is undefined
+console.log(typeof null);   // typeof of null is object
