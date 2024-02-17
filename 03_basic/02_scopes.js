5b906163245e8981256bbfe53d3877cf8dc7d985
Mitul Patil <119641618+MitulPatil@users.noreply.github.com>
2024-02-17 07:52:24 +0000

    scopes

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -12,3 +12,44 @@ console.log(a); // let can't be accessible outside of block
 // console.log(b); // const can't be accessible outside of block
 console.log(c); // var can access outside of block
 
+function one(){
+    const userneme = "mitul"
+
+    function two(){                       //child function can access members of parent function
+        const website = "javascript"
+        console.log(userneme);
+    }
+    //console.log(website);  // can't access outside of two() function
+
+    two()
+}
+
+one()
+
+if (true){
+    const username = "mitul"
+    if (username === "mitul"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+
+    //console.log(website);  // can't access here
+    console.log(username);
+}
+
+//console.log(username);  // can't access here
+
+// ******************************* interesting ****************************
+
+addone(4)
+
+function addone(num){
+    return num + 1
+}
+
+addtwo(3)
+
+const addtwo = function(num) {
+    return num + 2
+}
+
