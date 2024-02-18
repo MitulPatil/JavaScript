508c2106a6575e09c07bffea1c75574ae96ce1ad
Mitul Patil <119641618+MitulPatil@users.noreply.github.com>
2024-02-18 17:27:57 +0000

    trythly and falsy values

---
+++ 04_control_flow/truthly.js
@@ -0,0 +1,11 @@
+const useremail = ""
+
+if (useremail) {
+    console.log("got user email");
+} else{
+    console.log("Don't have user email");
+}
+
+// falsy values :- false, 0 ,-0, BigInt 0n, "", null, undefined, NaN
+
+// truthly values :- "0", "false" , " ", [], {}, function(){}
