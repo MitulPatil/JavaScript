94447f5ee71448af2082bac0d5858a84e81f40c3
Mitul Patil <119641618+MitulPatil@users.noreply.github.com>
2024-02-19 12:23:16 +0000

    for of loop

--- 05_iterations/for_of_loop.js
+++ 05_iterations/for_of_loop.js
@@ -1,5 +1,13 @@
-//for of
-//mostly used for array
+// for of
+// mostly used for array
+// The for...of statement executes a loop that operates on a sequence of values sourced from an iterable object. 
+// Iterable objects include instances of built-ins such as Array, String, TypedArray, Map, Set, NodeList (and other DOM collections), 
+// as well as the arguments object, generators produced by generator functions, and user-defined iterables.
+
+// synatx :- for (const iterator of object) {
+    
+// }
+
 
 const arr = [1, 2, 3, 4, 5]
 
