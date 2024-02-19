3cf0e203490474bc2cfb45e56c8a202402bef987
Mitul Patil <119641618+MitulPatil@users.noreply.github.com>
2024-02-19 05:23:23 +0000

    truthly and falsy values

--- 04_control_flow/truthly.js
+++ 04_control_flow/truthly.js
@@ -9,3 +9,40 @@ if (useremail) {
 // falsy values :- false, 0 ,-0, BigInt 0n, "", null, undefined, NaN
 
 // truthly values :- "0", "false" , " ", [], {}, function(){}
+
+// way to check array is empty or not
+const arr = []
+
+if (arr.length === 0) {
+    console.log("Array is empty");
+}
+
+// way to check object is empty or not
+
+const emptyobj = {}
+
+if (Object.keys(emptyobj).length === 0) {
+    console.log("object is empty");
+}
+
+// false == 0 -> true
+// false == "" -> true
+// 0 == "" -> true
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val;
+
+// val = 5 ?? 10
+val = null ?? 20
+// val = undefined ?? 15
+// val = null ?? 30 ?? 40
+
+console.log(val);
+
+// Terniary Operator (?:)
+// condition ? true : false
+
+const price = 100
+
+price >= 80 ? console.log("less than 80") : console.log("more than 80");
