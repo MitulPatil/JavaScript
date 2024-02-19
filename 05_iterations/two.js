22661ee92d22809342c189d7c34ca9884a653729
Mitul Patil <119641618+MitulPatil@users.noreply.github.com>
2024-02-19 07:51:09 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// while
+
+let index = 0
+
+while (index <= 10) {
+    console.log(`value os indecx is ${index}`);
+    index += 2
+}
+
+let myarr = ["ironman", "batman", "spiderman"]
+let arr = 0
+while (arr < myarr.length) {
+    console.log(`value is ${myarr[arr]}`);
+    arr += 1
+}
+
+//do while
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
